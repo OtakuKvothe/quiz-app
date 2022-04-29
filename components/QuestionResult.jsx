7267e0972a0c37ae4386352e1c305c870ed41acc
Swarnendu Basak <36073547+OtakuKvothe@users.noreply.github.com>
2022-04-30 01:41:18 +0530

    Create component for result

---
+++ components/QuestionResult.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const QuestionResult = ({ result }) => {
+
+    return(
+        <View style={styles.title}>
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 15
+    }
+});
+
+export default QuestionResult;
